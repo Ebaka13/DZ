91fff5275834f14626dcb96eda59d2dd64b5b37a
Sasha <lowkaomg@gmail.com>
2022-09-21 14:19:41 +0300

    Add home4

--- Home4/Program.cs
+++ Home4/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+if (num <= 1) Console.WriteLine("Чётные числа отсутствуют");
+if (num % 2 < 0) num = num - 1;
+else
+    while (count <= num)
+    {
+        Console.Write($"{count}, ");
+        count += 2;
+
+    }
