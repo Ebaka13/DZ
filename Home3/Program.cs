b4e4a360664a639bf5a4298d74183b2c4cdebbab
Sasha <lowkaomg@gmail.com>
2022-09-21 14:16:50 +0300

    Add home3

--- Home3/Program.cs
+++ Home3/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num >= 0)
+if(num % 2 > 0 ) Console.WriteLine($"{num} не является чётным");
+else 
+Console.WriteLine($"{num} является чётным");
+
+if(num < 0)
+if(-num % 2 > 0) Console.WriteLine($"{num} не является чётным");
+else 
+Console.WriteLine($"{num} является чётным");
