059f15e3300b359dca7a6a4684e6088b42cb95ae
Sasha <lowkaomg@gmail.com>
2022-09-25 13:10:14 +0300

    Add home6

--- Home6/Program.cs
+++ Home6/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Введи число: ");
+int Num = Convert.ToInt32(Console.ReadLine());
+string NumText = Convert.ToString(Num);
+if (NumText.Length > 2)
+{
+    Console.WriteLine("3 цифра -> " + NumText[2]);
+}
+else
+{
+    Console.WriteLine("-> 3 цифры нет");
+}
