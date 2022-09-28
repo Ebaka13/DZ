c2b4292b92f7f9ce5d00f9d09581b471cd6c7d85
Sasha <lowkaomg@gmail.com>
2022-09-28 16:55:51 +0300

    add home8

--- Home8/Program.cs
+++ Home8/Program.cs
@@ -1,23 +1,4 @@
-﻿// Console.WriteLine("Vvedite chislo");
-// int num = Convert.ToInt32(Console.WriteLine);
-
-// bool comparison(int num);
-// {
-//     int firstDigit = num / 10000 % 10;
-//     int secondDigit = num / 1000 % 10;
-//     int fourthDigit = num / 10 % 10;
-//     int fifthDigit = num % 10;
-//     return firstDigit == fifthDigit && secondDigit == fourthDigit;
-// }
-// if (number < 10000 || number > 99999) Console.WriteLine("Не корректные значения");
-// else
-// {
-//     if (comparison(num))
-//     Console.WriteLine("Yes");
-//     else 
-//     Console.WriteLine()
-// }
-Console.Write("Введите число: ");
+﻿Console.Write("Введите число: ");
 int numbers = Convert.ToInt32(Console.ReadLine());
 
 
