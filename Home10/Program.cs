518cf165e55b114be831d3c556fc812948078b1e
Sasha <lowkaomg@gmail.com>
2022-09-28 18:02:23 +0300

    add home10

---
+++ Home10/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int index = 1;
+
+void Cube(int num)
+{
+    while (index <= n)
+    {
+        int res = index * index * index;
+        Console.WriteLine($" | {index, 5}  |  {res, 5} |");
+        index++;
+    }
+}
+
+if (n > 0) Cube(n);
+else Console.WriteLine("Введите корректное знаечение");
