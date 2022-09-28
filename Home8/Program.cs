f3c43c1e5865d167ccd8b87996fd06623107298a
Sasha <lowkaomg@gmail.com>
2022-09-28 18:35:11 +0300

    reAdd Home8

--- Home8/Program.cs
+++ Home8/Program.cs
@@ -1,18 +1,14 @@
 ﻿Console.Write("Введите число: ");
 int numbers = Convert.ToInt32(Console.ReadLine());
-
-
 bool comparison(int num)
 {
-    int firstDigit = numbers / 10000 % 10;
+    int firstDigit = numbers / 10000 % 10;//1
     int secondDigit = numbers / 1000 % 10;
     int fourthDigit = numbers / 10 % 10;
     int fifthDigit = numbers % 10;
     return firstDigit == fifthDigit && secondDigit == fourthDigit;
-
-
 }
-if (numbers < 10000 || numbers > 99999) Console.WriteLine("Не пятизначное");
+if (numbers < 10000 || numbers > 99999) Console.WriteLine("Не пятизначное число");
 else
 {
     if (comparison(numbers)) Console.Write($"{numbers} -> Палиндром");
