85f7d60d7afbeee9df22d5bd27f85eea5e20f35d
Sasha <lowkaomg@gmail.com>
2022-10-04 12:00:06 +0300

    add home11

---
+++ Home11/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число A: ");
+int a =  Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int Degree(int numA, int numB)
+{
+    int sum = 1;
+    for(int i = 1; i <= numB; i++)
+    {
+        sum = sum * numA;
+    }
+    return sum;
+}
+if (b != 0)
+{
+    int result = Degree(a, b);
+    Console.WriteLine($"{a} ^ {b} = {result}");
+}
+else 
+Console.WriteLine($"{a} ^ {b} = 1");
