4c176e7e62671a30fddd95942dfa1df7deb8f89a
Sasha <lowkaomg@gmail.com>
2022-10-11 12:33:43 +0300

    add home34

---
+++ Home34/Program.cs
@@ -0,0 +1,34 @@
+﻿int[] CreateArrayRndInt(int size)
+{
+    int[] array = new int[size];
+    var rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(100, 1000);
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+int QuantityEvenNum(int[] array)
+{
+    int counter = default;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) counter++;
+    }
+    return counter;
+}
+int[] arr = CreateArrayRndInt(10);
+PrintArray(arr);
+int quantityEvenNum = QuantityEvenNum(arr);
+Console.WriteLine(quantityEvenNum);
