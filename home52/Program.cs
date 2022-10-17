4cb1279a27dfc2f7e0b3a608d4b34e02fd0fe6fc
Sasha <lowkaomg@gmail.com>
2022-10-17 19:49:33 +0300

    add home52

---
+++ home52/Program.cs
@@ -0,0 +1,58 @@
+﻿Console.WriteLine("Введите число строк");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число столбцов");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    var rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4}, ");
+            else Console.Write($"{matrix[i, j],4}");
+        }
+        Console.WriteLine("]");
+    }
+}
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+void ArithmeticalMeanColumn(int[,] matrix)
+{
+    double arithmeticalMeanColumn = 0;
+    double sumColumns = 0;
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            sumColumns += matrix[i, j];
+        }
+        arithmeticalMeanColumn = sumColumns / matrix.GetLength(0);
+        Console.WriteLine($"Среднеарифметическое {j + 1} столбца = {arithmeticalMeanColumn}");
+        sumColumns = 0;
+    }
+}
+int[,] array2D = CreateMatrixRndInt(m, n, -10, 10);
+PrintMatrix(array2D);
+ArithmeticalMeanColumn(array2D);
