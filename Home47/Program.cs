8dc9d9fa0364c8b30df13f32b225a2c2a45d6c5e
Sasha <lowkaomg@gmail.com>
2022-10-17 19:49:56 +0300

    add home47

---
+++ Home47/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("введите количество строк матрицы: m");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите количество столбцов матрицы: n");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] CreateMatrixIndex(int rows, int columns)
+{
+    double[,] matrix = new double[rows, columns];
+    var rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)   
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Math.Round((rnd.NextDouble()*10), 1, MidpointRounding.ToZero);
+        }
+    }
+    return matrix;
+}
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[  ");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j]}, ");
+            else Console.Write($"{matrix[i, j],4}");
+        }
+        Console.WriteLine(" ]");
+    }
+}
+double[,] arrayInd = CreateMatrixIndex(m, n);
+PrintMatrix(arrayInd);
