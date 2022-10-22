48d5efede7bea908ed4af640c7db58771376cbbb
Sasha <lowkaomg@gmail.com>
2022-10-22 12:26:29 +0300

    add home66

---
+++ Home64/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+NaturalNumbers(number);
+
+void NaturalNumbers(int num)
+{
+    if (num == 0) return;
+    Console.Write($"{num} ");
+    NaturalNumbers(num - 1);
+}
