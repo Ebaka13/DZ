ebe1c7b1fd7e2b19879bd1400d0bf6db193087db
Sasha <lowkaomg@gmail.com>
2022-10-22 12:27:16 +0300

    add home66

---
+++ Home66/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число: ");
+int numF = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numS = Convert.ToInt32(Console.ReadLine());
+int Sum(int numF, int numS)
+{
+    if (numF < numS) return numS + Sum(numF, numS - 1);
+    if (numF > numS) return numS + Sum(numF, numS + 1);
+    return numF;
+}
+int result = Sum(numF, numS);
+Console.WriteLine($"Сумма натуральных элементов в промежутке от {numF} до {numS} -> {result}");
